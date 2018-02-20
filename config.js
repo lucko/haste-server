b8b0672865c33a5e930a916328c544639d05a097
Luck <git@lucko.me>
2018-02-20 00:06:43 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 {
 
   "host": "0.0.0.0",
-  "port": 7777,
+  "port": 80,
 
   "keyLength": 10,
 
@@ -33,11 +33,7 @@
   },
 
   "storage": {
-    "type": "redis",
-    "host": "0.0.0.0",
-    "port": 6379,
-    "db": 2,
-    "expire": 2592000
+    "type": "file"
   },
 
   "documents": {
